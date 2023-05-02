ef3fcc578183ef7a40c420d999ebdf64efeba2f4
seabeach11 <seebsk9@gmail.com>
2023-05-02 18:04:36 +0600

    added btn

--- src/main/Header.jsx
+++ src/main/Header.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "./Provider/AuthProvider";
 
 const Header = () => {
-    return (
-        <div>
-            <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered" />
-    </div>
-        <Link to="/register">Register</Link>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  const { user } = useContext(AuthContext);
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
         </div>
-      </label>
-      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered"
+            />
+          </div>
+          <Link to="/register">Register</Link>
         </div>
-    );
+
+        {user?.email ? (
+          <li>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        ) : (
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+        )}
+       
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/main/Provider/AuthProvider.jsx
+++ src/main/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import app from "./firebase.config";
 
 
@@ -7,13 +7,23 @@ import app from "./firebase.config";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);  
  const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState({});
   
    const registerUser = (name,email,password)=>{
     return createUserWithEmailAndPassword(auth ,name, email, password)
 
    } 
+
+   useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (loggedInUser) => {
+      setUser(loggedInUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
     
-    const authInfo = {registerUser};
+    const authInfo = {registerUser,user};
     
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
